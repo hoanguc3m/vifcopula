5a677de22e86179a60e32c57be820881853585db
hoanguc3m <nhoang.neu@gmail.com>
2017-04-28 11:47:15 +0200

    add hfunc

--- inst/include/transform/hfunc_frank.hpp
+++ inst/include/transform/hfunc_frank.hpp
@@ -1,120 +1,76 @@
-#ifndef VIFCOPULA_DISTRIBUTION_BICOP_FRANK_LOG_HPP
-#define VIFCOPULA_DISTRIBUTION_BICOP_FRANK_LOG_HPP
+#ifndef VIFCOPULA_HFUNC_FRANK_HPP
+#define VIFCOPULA_HFUNC_FRANK_HPP
 
 #include <stan/math.hpp>
 
-namespace vifcopula {
+
+namespace vifcopula
+{
 
 using namespace stan::math;
 using namespace stan;
 
-    /**
-     * The log of the bivariate Frank copula density for the specified vector(s) u
-     * and the specified vector(s) v given degree(s) of dependence theta.
-     *  u, v, or theta can each be either a scalar or a vector. Any vector inputs
-     * must be the same length.
-     *
-     * <p>The result log probability is defined to be the sum of the
-     * log probabilities for each observation/observation/dependence.
-     * @param u (Sequence of) scalar(s) in [0,1].
-     * @param v (Sequence of) scalar(s) in [0,1].
-     * @param theta (Sequence of) dependence parameters for the Frank copula
-     * @return The log of the product of the densities.
-     * @tparam T_u Underlying type of scalar in sequence.
-     * @tparam T_v Underlying type of scalar in sequence.
-     * @tparam T_theta Type of dependence parameter.
-     */
+/**
+ * The Conditional Distribution Function of a Bivariate Frank Copula
+ * for the specified vector(s) u and the specified vector(s) v given parameter(s) theta.
+ * u, v, or theta can each be either a scalar or a vector. Any vector inputs
+ * must be the same length.
+ *
+ * <p>The result of h2 function is defined in VineCopula package.
+ * @param u (Sequence of) scalar(s) in [0,1].
+ * @param v (Sequence of) scalar(s) in [0,1].
+ * @param theta (Sequence of) parameters for the Frank copula
+ * @return The Conditional Distribution Function of a bivariate Frank Copula of u given v
+ * @tparam T_u Underlying type of scalar in sequence.
+ * @tparam T_v Underlying type of scalar in sequence.
+ * @tparam T_theta Type of correlation parameter.
+ */
 
-    template <bool propto, typename T_u, typename T_v, typename T_theta>
-    typename stan::return_type<T_u, T_v, T_theta>::type
-    bicop_frank_log(const T_u& u, const T_v& v, const T_theta& theta) {
-      static const char* function("vifcopula::bicop_frank_log");
-      typedef typename stan::partials_return_type<T_u, T_v, T_theta>::type
-        T_partials_return;
+template <typename T_u, typename T_v, typename T_theta>
+inline
+T_u hfunc_frank(const T_u& u, const T_v& v, const T_theta& theta)
+{
+    static const char* function("vifcopula::hfunc_frank");
 
       using std::log;
       using std::pow;
       using std::exp;
 
-      if (!(stan::length(u)
-            && stan::length(v)
-            && stan::length(theta)))
-        return 0.0;
-
-      T_partials_return logp(0.0);
-
 //      check_bounded(function, "Random variable u", u,0,1);
 //      check_bounded(function, "Random variable v", v,0,1);
-//      check_positive(function, "dependence parameter", square(theta) ); // theta^2 > 0
-      check_consistent_sizes(function,
-                             "Random variable u", u,
-                             "Random variable v", v,
-                             "Scale parameter", theta);
-      if (!include_summand<propto, T_u, T_v, T_theta>::value)
-        return 0.0;
+//      check_bounded(function, "Correlation parameter", theta, -1,1);
+    check_consistent_sizes(function,
+                           "Random variable u", u,
+                           "Random variable v", v,
+                           "Scale parameter", theta);
 
-      OperandsAndPartials<T_u, T_v, T_theta> operands_and_partials(u, v, theta);
+    stan::VectorView<const T_u> u_vec(u);
+    stan::VectorView<const T_v> v_vec(v);
+    stan::VectorView<const T_theta> theta_vec(theta);
+    size_t N = stan::max_size(u, v, theta);
 
-      stan::VectorView<const T_u> u_vec(u);
-      stan::VectorView<const T_v> v_vec(v);
-      stan::VectorView<const T_theta> theta_vec(theta);
+    T_u u_cond(u);
 
-      size_t N = stan::max_size(u, v, theta);
+    for (size_t n = 0; n < N; n++)
+    {
+        double u_val = value_of(u_vec[n]);
+        double v_val = value_of(v_vec[n]);
+        double theta_value = value_of(theta_vec[n]);
+        double exp_theta = exp(theta_value);
+        double exp_theta_u = pow( exp_theta, u_val);
+        double exp_theta_v= pow( exp_theta, v_val);
+        double exp_theta_uv= exp_theta_u * exp_theta_v;
+        double exp_theta_up1 = exp_theta_u * exp_theta;
+        double exp_theta_vp1 = exp_theta_v * exp_theta;
 
-      stan::VectorBuilder<true, T_partials_return, T_theta> theta_value(length(theta));
-      stan::VectorBuilder<true, T_partials_return, T_theta> exp_theta(length(theta));
-      stan::VectorBuilder<true, T_partials_return, T_theta> log_theta(length(theta));
+        u_cond[n] = - exp_theta * (exp_theta_v -1) / (exp_theta_uv - exp_theta_up1 - exp_theta_vp1 + exp_theta);
 
-      for (size_t i = 0; i < length(theta); i++) {
-        theta_value[i] = value_of(theta_vec[i]);
-        exp_theta[i] = exp(theta_value[i]);
-        log_theta[i] = log(theta_value[i]);
-      }
-
-
-      for (size_t n = 0; n < N; n++) {
-        const T_partials_return u_dbl = value_of(u_vec[n]);
-        const T_partials_return v_dbl = value_of(v_vec[n]);
-        const T_partials_return exp_theta_u = pow( exp_theta[n], u_dbl);
-        const T_partials_return exp_theta_v= pow( exp_theta[n], v_dbl);
-        const T_partials_return exp_theta_uv= exp_theta_u * exp_theta_v;
-        const T_partials_return exp_theta_up1 = exp_theta_u * exp_theta[n];
-        const T_partials_return exp_theta_vp1 = exp_theta_v * exp_theta[n];
-        const T_partials_return exp_theta_uvsum = exp_theta_uv - exp_theta_up1 - exp_theta_vp1 + exp_theta[n];
-
-        // Calculate the likelihood of Frank copula
-        // if (include_summand<propto>::value)
-        //   logp = 0;
-        if (include_summand<propto, T_theta>::value)
-            logp += log_theta[n] + log(exp_theta[n]-1);
-
-
-        if (include_summand<propto, T_u, T_v, T_theta>::value)
-          logp += theta_value[n] * (u_dbl + v_dbl+1) -
-                    2 * log(abs( exp_theta_uvsum )) ;
-         // Calculate the derivative when the type is var (not double)
-         if (!is_constant_struct<T_u>::value)
-            operands_and_partials.d_x1[n] += theta_value[n] - 2 * theta_value[n] * ( exp_theta_uv - exp_theta_up1) / exp_theta_uvsum ;
-         if (!is_constant_struct<T_v>::value)
-            operands_and_partials.d_x2[n] += theta_value[n] - 2 * theta_value[n] * (exp_theta_uv - exp_theta_vp1) / exp_theta_uvsum ;
-         if (!is_constant_struct<T_theta>::value)
-            operands_and_partials.d_x3[n] += 1/ theta_value[n] + exp_theta[n] / (exp_theta[n] - 1) +
-                                            (u_dbl + v_dbl+1) -
-                                            2 * (exp_theta_uv * (u_dbl+v_dbl)- exp_theta_up1*(u_dbl+1) - exp_theta_vp1*(v_dbl+1) + exp_theta[n] )/
-                                                exp_theta_uvsum;
-
-      }
-      return operands_and_partials.value(logp);
     }
+    return u_cond;
+}
 
-    template <typename T_u, typename T_v, typename T_theta>
-    inline
-    typename stan::return_type<T_u, T_v, T_theta>::type
-    bicop_frank_log(const T_u& u, const T_v& v, const T_theta& theta) {
-      return bicop_frank_log<false>(u, v, theta);
-    }
 
 }
-#endif // VIFCOPULA_DISTRIBUTION_BICOP_FRANK_LOG_HPP
+#endif // VIFCOPULA_HFUNC_FRANK_HPP
 
 
--- inst/include/unitTests/hfunc_test.cpp
+++ inst/include/unitTests/hfunc_test.cpp
@@ -3,6 +3,10 @@
 #include <transform/hfunc_independence.hpp>
 #include <transform/hfunc_normal.hpp>
 #include <transform/hfunc_student.hpp>
+#include <transform/hfunc_clayton.hpp>
+#include <transform/hfunc_gumbel.hpp>
+#include <transform/hfunc_frank.hpp>
+#include <transform/hfunc_joe.hpp>
 
 #include <iostream>
 
@@ -36,6 +40,54 @@ TEST(Hfunc, Hfunc_StuCop) {
     double nu = 5;
 
     std::vector<double> u_cond = vifcopula::hfunc_student(u, v, rho, nu);
+    //std::cout << u_cond[0] << std::endl;
+    EXPECT_FLOAT_EQ(u_cond[0],u_cond_true[0]);
+}
+
+TEST(Hfunc, Hfunc_ClaytonCop) {
+
+    std::vector<double> v = {0.8};
+    std::vector<double> u = {1.0};
+    std::vector<double> u_cond_true = {0.4096};
+    double theta = 3;
+
+    std::vector<double> u_cond = vifcopula::hfunc_clayton(u, v, theta);
+    //std::cout << u_cond[0] << std::endl;
+    EXPECT_FLOAT_EQ(u_cond[0],u_cond_true[0]);
+}
+
+TEST(Hfunc, Hfunc_GumbelCop) {
+
+    std::vector<double> v = {0.8};
+    std::vector<double> u = {0.7};
+    std::vector<double> u_cond_true = {0.8411075};
+    double theta = 3;
+
+    std::vector<double> u_cond = vifcopula::hfunc_gumbel(u, v, theta);
+    //std::cout << u_cond[0] << std::endl;
+    EXPECT_FLOAT_EQ(u_cond[0],u_cond_true[0]);
+}
+
+TEST(Hfunc, Hfunc_FrankCop) {
+
+    std::vector<double> v = {0.8};
+    std::vector<double> u = {0.7};
+    std::vector<double> u_cond_true = {0.73121};
+    double theta = 3;
+
+    std::vector<double> u_cond = vifcopula::hfunc_frank(u, v, theta);
+    //std::cout << u_cond[0] << std::endl;
+    EXPECT_FLOAT_EQ(u_cond[0],u_cond_true[0]);
+}
+
+TEST(Hfunc, Hfunc_JoeCop) {
+
+    std::vector<double> v = {0.8};
+    std::vector<double> u = {0.7};
+    std::vector<double> u_cond_true = {0.837853};
+    double theta = 3;
+
+    std::vector<double> u_cond = vifcopula::hfunc_joe(u, v, theta);
     //std::cout << u_cond[0] << std::endl;
     EXPECT_FLOAT_EQ(u_cond[0],u_cond_true[0]);
 }
