c87dbb267040b859c155083f9e4f2b9861c876b5
hoanguc3m <hoang.nguyen@uc3m.es>
2020-07-08 09:09:26 +0200

    Update

--- vignettes/sim/hmc_bifcop.R
+++ vignettes/sim/hmc_bifcop.R
@@ -174,7 +174,7 @@ latentcopfamily = sample(c(1,3,4,5),size = n_max, replace = T)
 
 datagen_mix <- fcopsim(t_max = 1000, n_max = 100, k_max = k_max, gid = gid,
                        family = copfamily, family_latent = latentcopfamily,
-                       seed_num = 0, structfactor = 2)
+                       seed_num = 100, structfactor = 2)
 tail(BiCopPar2Tau(family = datagen_mix$family, par = datagen_mix$theta, par2 = datagen_mix$theta2),10)
 
 datagen <- datagen_mix
@@ -186,8 +186,8 @@ data <- list(u = datagen$u,
              structfactor = datagen$structfactor)
 init <- list(copula_type = datagen$family,
              latent_copula_type = datagen$family_latent)
-other <- list(seed = 126, core = 8, num_warmup = 500, num_samples = 1000, copselect = F,
-              tol_rel_obj = 0.001)
+other <- list(seed = 126, core = 8, num_warmup = 2000, num_samples = 1000, copselect = F,
+              tol_rel_obj = 0.005)
 hmc_mix <- vifcopula::hmcfcop(data,init,other)
 vi_mix <- vifcopula::vifcop(data,init,other)
 
@@ -384,8 +384,8 @@ dev.off()
 ###############################################################################
 hmc_col <- ncol(hmc_mix$sample_hmc)
 hmc_sample <- hmc_mix$sample_hmc[,8:hmc_col]
-v_idx <- c(100,500, 900)
-theta_idx <- c(10,40, 90)
+v_idx <- c(100,700, 1000)
+theta_idx <- c(10,101, 102)
 
 paranum <- cumsum(rep(1,100) + (datagen_mix$family == 2) + (datagen_mix$family == 7))
 a1 <- which( paranum == theta_idx[1]);  datagen_mix$family[a1] # check here
@@ -393,14 +393,18 @@ a2 <- which( paranum == theta_idx[2]);  datagen_mix$family[a2] # check here
 a3 <- which( paranum == theta_idx[3]);  datagen_mix$family[a3] # check here
 theta_sample_idx <- k_max * t_max + paranum[c(a1,a2,a3)] # check here
 
+theta_sample_idx <- k_max * t_max + theta_idx # check here
+
 hmc_sample_select <- hmc_sample[,c(v_idx,theta_sample_idx)]
 vi_sample_select <- vi_mix$sample_vi[,c(v_idx,theta_sample_idx)]
 
 labels_ii = c(expression('v'[100]),expression('v'[500]), expression('v'[900]),
-              expression(theta[10]),expression(theta[50]), expression(theta[90]))
+              expression(theta[10]^G[p]),expression(theta[90]^BB1), expression(delta[90]^BB1))
 
 
 true_val <- c(datagen_mix$v[v_idx], datagen_mix$theta[a1], datagen_mix$theta[a2], datagen_mix$theta[a3])
+true_val <- c(datagen_mix$v[v_idx], datagen_mix$theta[a1], datagen_mix$theta[a3], datagen_mix$theta2[a3])
+true_val
 apply(hmc_sample_select, MARGIN = 2, FUN = mean)
 apply(vi_sample_select, MARGIN = 2, FUN = mean)
 
@@ -473,6 +477,93 @@ for (i in c(1:maxplot)){
 dev.off()
 
 par(mfrow=c(1,1))
+
+####################################################################
+# Gumbel and Student
+####################################################################
+hmc_col <- ncol(hmc_mix$sample_hmc)
+hmc_sample <- hmc_mix$sample_hmc[,8:hmc_col]
+v_idx <- c(100,700, 1000)
+theta_idx <- c(1,2, 3)
+
+paranum <- cumsum(rep(1,100) + (datagen_mix$family == 2) + (datagen_mix$family == 7))
+a1 <- which( paranum == theta_idx[1]);  datagen_mix$family[a1] # check here
+a2 <- which( paranum == theta_idx[2]);  datagen_mix$family[a2] # check here
+a3 <- which( paranum == theta_idx[3]);  datagen_mix$family[a3] # check here
+theta_sample_idx <- k_max * t_max + paranum[c(a1,a2,a3)] # check here
+
+theta_sample_idx <- k_max * t_max + theta_idx # check here
+
+hmc_sample_select <- hmc_sample[,c(v_idx,theta_sample_idx)]
+vi_sample_select <- vi_mix$sample_vi[,c(v_idx,theta_sample_idx)]
+
+labels_ii = c(expression('v'[100]),expression('v'[500]), expression('v'[900]),
+              expression(theta[50]),expression(theta[80]^1), expression(theta[80]^2))
+
+
+true_val <- c(datagen_mix$v[v_idx], datagen_mix$theta[a1], datagen_mix$theta[a2], datagen_mix$theta[a3])
+true_val <- c(datagen_mix$v[v_idx], datagen_mix$theta[a1], datagen_mix$theta[a3], datagen_mix$theta2[a3])
+true_val
+apply(hmc_sample_select, MARGIN = 2, FUN = mean)
+apply(vi_sample_select, MARGIN = 2, FUN = mean)
+
+# pdf(file='img/scatterHMCVIv2.pdf', width = 8, height = 7)
+
+
+library("KernSmooth")
+i = 1; j = 2
+maxplot = 6
+par(mfrow = c(maxplot,maxplot))
+par(mar=c(2,2,0,0))
+library(RColorBrewer)
+k <- 11
+colfunc_hmc <- colorRampPalette(c(rgb(0,0.2,1,0.5), rgb(0,0.2,1,1)), alpha = TRUE)
+colfunc_vi <- colorRampPalette(c(rgb(1,0,0,0.25), rgb(1,0,0,0.75)), alpha = TRUE)
+my.cols <- colfunc_hmc(k)
+my.cols.vi <- colfunc_vi(k)
+
+
+for (i in c(1:maxplot)){
+        for (j in c(1:maxplot)){
+                if (i == j) {
+                        plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
+                        text(x = 0.4, y = 0.25, labels_ii[i],
+                             cex = 2.5, col = "black")
+                }
+                if (i < j) {
+                        plot.new()
+                }
+                if (i > j) {
+                        xaxt_n = 'n'
+                        yaxt_n = 'n'
+                        if (i == maxplot) {xaxt_n = 's'}
+                        if (j == 1) {yaxt_n = 's'}
+                        x <- data.frame(X = hmc_sample_select[,j], Y = hmc_sample_select[,i])
+                        est <- bkde2D(x, bandwidth=sapply(x,dpik)*1.4,
+                                      range.x=list(c(min(x[,1]),max(x[,1])),
+                                                   c(min(x[,2]),max(x[,2]))))
+
+                        contour(est$x1, est$x2, est$fhat,
+                                drawlabels = F, nlevels = 7, lty = 2, lwd = 1.5,
+                                yaxt=yaxt_n, xaxt=xaxt_n,
+                                col = my.cols)
+                        points(true_val[j], true_val[i], pch = 8, col = "black", cex = 2)
+
+                        x <- data.frame(X = vi_sample_select[,j], Y = vi_sample_select[,i])
+                        est <- bkde2D(x, bandwidth=sapply(x,dpik)*1.4,
+                                      range.x=list(c(min(x[,1]),max(x[,1])),
+                                                   c(min(x[,2]),max(x[,2]))))
+                        contour(est$x1, est$x2, est$fhat,
+                                drawlabels = F, nlevels = 7, col=my.cols.vi,
+                                add=TRUE, lwd = 1.5)
+
+
+
+                }
+        }
+}
+dev.off()
+
 
 ###############################################################################
 # pdf(file='img/VIvsHMCbifcop.pdf', width = 18, height = 6)
